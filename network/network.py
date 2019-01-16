f8e435690529c055a4f9891a22a2fbdbc0ace50e
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-01-16 22:04:54 +0100

    first methods

--- network/network.py
+++ network/network.py
@@ -1,6 +1,14 @@
-"""Tjis module contains class of network."""
+"""This module contains class of network."""
 class NeuralNetwork:
     """
     This is class which stores all information about the network
     and has its state.
     """
+    def __init__(self):
+        pass
+    def set_input_size(self, size):
+        """This method sets quantity of input neurons."""
+    def set_hidden_size(self, size):
+        """This method sets size of hidden layer. Nobody expected it."""
+    def set_output_size(self, size):
+        """xD"""
