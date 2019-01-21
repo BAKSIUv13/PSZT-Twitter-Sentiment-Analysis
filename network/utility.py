2171c880b9d4e9ff9abaac75c9fec5e53376ddca
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-01-21 15:31:07 +0100

    rzeczs

--- network/utility.py
+++ network/utility.py
@@ -19,3 +19,26 @@ def full_learning(
     """This function fires a loop that teaches our network with given set."""
     for _ in range(iterations_quantity):
         net.learn(input_sets, expectations, speed)
+
+def load_from_file(
+        net,
+        file_path
+        ):
+    """This function load enitire weight vector to network from file."""
+    serialized = []
+    with open(file_path, 'r') as the_file:
+        file_content = the_file.readlines()
+    for line in file_content:
+        curr = line[:-1]
+        serialized.append(float(curr))
+    net.paste_serialized_weights(serialized)
+
+def save_to_file(
+        net,
+        file_path
+        ):
+    """This function saves state of the network (only weight values)
+to file."""
+    serialized = net.serialize_weights()
+    with open(file_path, 'w') as the_file:
+        the_file.writelines("%s\n" % number for number in serialized)
